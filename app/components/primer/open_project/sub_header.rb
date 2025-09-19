395d297655c3089d68f96a5fa546f8ee87414109
Eric-Guo <eric.guocz@gmail.com>
2025-09-19 11:25:53 +0800

    feat(SubHeader): support  and render in correct pane; default remains middle; left text hidden when filter expands on mobile

--- app/components/primer/open_project/sub_header.rb
+++ app/components/primer/open_project/sub_header.rb
@@ -248,11 +248,23 @@ module Primer
         Primer::OpenProject::SubHeader::SegmentedControl.new(display: DESKTOP_ACTIONS_DISPLAY, **system_arguments)
       }
 
-      renders_one :text, lambda { |**system_arguments|
+      renders_one :text, lambda { |location: :middle, **system_arguments, &block|
         system_arguments[:font_weight] ||= :bold
         system_arguments[:mx] ||= 2
+        system_arguments = set_as_hidden_filter_target(system_arguments) if location == :left
 
-        Primer::Beta::Text.new(**system_arguments)
+        component = Primer::Beta::Text.new(**system_arguments)
+
+        case location
+        when :left
+          @left_text = { component: component, block: block }
+        when :right
+          @right_text = { component: component, block: block }
+        else
+          @middle_text = { component: component, block: block }
+        end
+
+        component
       }
 
       # A slot for a generic component which will be shown in a second row below the rest, spanning the whole width
@@ -307,7 +319,7 @@ module Primer
 
         @system_arguments[:classes] = class_names(
           @system_arguments[:classes],
-          "SubHeader--emptyLeftPane" => !segmented_control? && !filter_button && !filter_input && all_quick_actions.empty? && @left_actions.empty?
+          "SubHeader--emptyLeftPane" => !segmented_control? && !filter_button && !filter_input && all_quick_actions.empty? && @left_actions.empty? && @left_text.nil?
         )
       end
 
